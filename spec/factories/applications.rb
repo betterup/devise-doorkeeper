0299a4ae1522a8ea10dc2a27c90c01a953c5f8e3
Bryan Hickerson <8365065+bryanhickerson@users.noreply.github.com>
2021-03-01 16:01:17 -0800

    Switch to FactoryBot from FactoryGirl
    https://thoughtbot.com/blog/factory_bot

--- spec/factories/applications.rb
+++ spec/factories/applications.rb
@@ -1,8 +1,6 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :application, class: Doorkeeper::Application do
-    name 'sample app'
-    # uid { SecureRandom.hex(20) }
-    # secret { SecureRandom.hex(20) }
-    redirect_uri 'urn:ietf:wg:oauth:2.0:oob'
+    name { 'sample app' }
+    redirect_uri { 'urn:ietf:wg:oauth:2.0:oob' }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   sequence(:email) { "jon.doe+#{SecureRandom.hex(10)}@acme.com" }
 
   factory :user do
